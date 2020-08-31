4cf0c7266e8541d805d231a1df9296aa6134bd39
LisaHJung <lisahjung@gmail.com>
2020-08-31 13:07:12 -0600

    Install nodemon and express framework. Import http core module and express third party framework. Create server and have the server listen at port 3000.

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) => {
+    console.log('In the middleware!')
+})
+
+const server = http.createServer()
+
+server.listen(3000)
