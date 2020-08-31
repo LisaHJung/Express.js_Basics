f9e0648398e30445fb78fca2c98d34d4c2f40d76
LisaHJung <lisahjung@gmail.com>
2020-08-31 13:37:21 -0600

    Replace createServer function and listen function with app.listen(3000). Remove require function for http as Express app.listen() does all of that for us.

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
@@ -14,6 +12,4 @@ app.use((req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000)
