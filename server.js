fbaf9a09150586e526c36f6097373b6ce9fdf33e
LisaHJung <lisahjung@gmail.com>
2020-08-31 14:06:25 -0600

    Add middleware for / and add-product. Configure Middleware in a way so that if a request has /, print -This always runs in the console= and move on to the next middleware(next()). Add a middleware /add-product. This will send a reponse to the page and print -In another middlewar-- in the console. The result shows that when a request is sent from localhost:3000/add-product, it hits the first middleware because the request contains / and because of next() the next middleware with matching /add-product is executed. Replace the http module, createServer(), and listen with  app.listen(3000)

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); 
+app.use('/', (req, res, next) => {
+    console.log('This always runs!')
+    next()
+})
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Add Product Page!</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
