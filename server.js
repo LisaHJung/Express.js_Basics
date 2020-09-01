423ffdb4787e2b47c3042c22e81fb2ffbf06d795
LisaHJung <lisahjung@gmail.com>
2020-09-01 12:06:01 -0600

    Create 404 html file and configure original catch all function created for page not found to send 404 html file instead.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,7 +13,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1> Page not found</h1>')
+  res.status(404).sendFile(path.join(__dirname, "Views", "404.html"));
 })
 
 app.listen(3000);
