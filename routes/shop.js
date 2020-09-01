3cbf37e871254d04345fe3866c7896c5969070db
LisaHJung <lisahjung@gmail.com>
2020-09-01 12:25:37 -0600

    Create a util folder, within it, create path.js file. Write a helper function to get to the parent directory of a file responsible for running the app. path.dirname(process.mainModule.filename). This process eliminates the need of writing __dirName, '../' in path.join when sending a file to the browser.

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
-const path = require('path')
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
+
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'Views', 'shop.html'))
+router.get("/", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "Views", "shop.html"));
 });
 
 module.exports = router;
